eb43d4fc5b97b31337d32f1600d88ed34ca7cdff
viet2005-68 <23020170@vnu.edu.vn>
2026-03-20 14:15:17 +0700

    first commit

---
+++ src/test/java/ShippingCalculatorBoundaryTest.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.viet.ShippingCalculator;
+public class ShippingCalculatorTest {
+
+    @Test
+    public void test1_NomNom() {
+        assertEquals(245000, ShippingCalculator.calculateFee(100, 25.0));
+    }
+
+    @Test
+    public void test2_MinNom() {
+        assertEquals(65000, ShippingCalculator.calculateFee(1, 25.0));
+    }
+
+    @Test
+    public void test3_MinPlusNom() {
+        assertEquals(65000, ShippingCalculator.calculateFee(2, 25.0));
+    }
+
+    @Test
+    public void test4_MaxMinusNom() {
+        assertEquals(443000, ShippingCalculator.calculateFee(199, 25.0));
+    }
+
+    @Test
+    public void test5_MaxNom() {
+        assertEquals(445000, ShippingCalculator.calculateFee(200, 25.0));
+    }
+
+    @Test
+    public void test6_NomMin() {
+        assertEquals(195000, ShippingCalculator.calculateFee(100, 0.1));
+    }
+
+    @Test
+    public void test7_NomMinPlus() {
+        assertEquals(195000, ShippingCalculator.calculateFee(100, 0.2));
+    }
+
+    @Test
+    public void test8_NomMaxMinus() {
+        assertEquals(245000, ShippingCalculator.calculateFee(100, 49.9));
+    }
+
+    @Test
+    public void test9_NomMax() {
+        assertEquals(245000, ShippingCalculator.calculateFee(100, 50.0));
+    }
+}
